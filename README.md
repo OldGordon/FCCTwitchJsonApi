54b0e5cc3d3576d03db740447e4ae1edc67aac5c
OldGordon <grindersan@gmail.com>
2016-02-16 09:49:00 +0100

    Arreglando jhint errors

--- README.md
+++
@@ -1,2 +0,0 @@
-# FCCTwitchJsonApi
-FCC zipline Twitch Json Api
--- app/app.js
+++ app/app.js
@@ -1,10 +1,35 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
+var myApp = myApp || {};
+
 $(document).ready(function () {
-    'use strict';
+    //'use strict';
 
 
+	var TwitchUser = function () {
+		this.channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx",
+                    "RobotCaleb", "thomasballinger", "noobs2ninjas",
+                    "beohoff", "brunofin", "comster404"];
+	};
 
+	TwitchUser.prototype = (function () {
+		var	url = "",
+
+			getData = function (prop, chan) {
+				for (var i = 0 ; i < this.channels.length; i++){
+					url = "https://api.twitch.tv/kraken/" + prop + "/" +          chan + "?callback=?";
+						return url;
+				}
+			},
+
+			getFile = function () {
+				$.getJSON(getData("streams", channel), function(dt){
+
+
+				})
+		    };
+
+	}());
 
 
 });
